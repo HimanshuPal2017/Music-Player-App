c9fad2c524996d31e5c205d39cc48deacf328cb6
HimanshuPal2017 <palh2593@gmail.com>
2024-09-10 10:09:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let progress = document.querySelector("#progress");
+let song = document.querySelector("#music");
+let controlIcon = document.querySelector("#ctrlIcon");
+
+song.onloademetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (controlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    controlIcon.classList.remove("fa-pause");
+    controlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    controlIcon.classList.add("fa-pause");
+    controlIcon.classList.remove("fa-play");
+  }
+}
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+};
